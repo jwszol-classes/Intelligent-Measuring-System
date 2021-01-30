198833d6c06b87ef8db3130ed70d5ebc7ed26fac
Karol Damaszke <karidamaszke@gmail.com>
2021-01-30 15:51:54 +0100

    Add pure-python version

--- utils/collect_data.py
+++ utils/collect_data.py
@@ -1,22 +1,24 @@
-import cv2
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 from itertools import product
 from math import log, tan, pi
+import shutil
 
 import boto3
 import botocore
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import skimage.measure
 from botocore import UNSIGNED
 from botocore.config import Config
 
-BUCKET_NAME = 'elevation-tiles-prod'   # replace with your bucket name
-KEY = 'terrarium/{z}/{x}/{y}.png'    # replace with your object key
+BUCKET_NAME = 'elevation-tiles-prod'
+KEY = 'terrarium/{z}/{x}/{y}.png'
 
-s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+ZOOM = 4
+BOUNDS = (72.0, -168.0, -54.0, -25.0)   # North and South America
 
-BOUNDS = (72.0, -168.0, -54.0, -25.0)
-ZOOM = 3
+OUTPUT_PATH = 'out'
 
 
 def mercator(lat, lon, zoom):
@@ -56,14 +58,17 @@ def get_tiles(zoom, lat1, lon1, lat2, lon2):
 
 
 def download(tiles):
-    output_path = 'out'
+    """
+    Download given tiles from S3 bucket
+    """
     try:
-        for i, (z, x, y) in enumerate(tiles):
+        s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
 
-            print("Downloading " + str(i+1) + "/" + str(len(tiles)))
+        for i, (z, x, y) in enumerate(tiles):
+            print("Downloading " + str(i + 1) + "/" + str(len(tiles)))
 
             url = KEY.format(z=z, x=x, y=y)
-            s3.Bucket(BUCKET_NAME).download_file(url, '{}\\{}-{}-{}.png'.format(output_path, z, x, y))
+            s3.Bucket(BUCKET_NAME).download_file(url, '{}\\{}-{}-{}.png'.format(OUTPUT_PATH, z, x, y))
 
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
@@ -73,44 +78,148 @@ def download(tiles):
 
     except Exception as ex:
         print("Exception occurred: " + str(ex))
+        raise
 
 
-def plot_all():
-    os.chdir('out')
-    images = os.listdir()
-    for img in images:
-        print(img)
-        im = cv2.imread(img, cv2.IMREAD_UNCHANGED)
-        height_arr = np.zeros(im.shape[:2])
-        for row in range(im.shape[0]):
-            for column in range(im.shape[1]):
-                bgr = im[row][column]
-                height = (bgr[2] * 256.0 + bgr[1] + bgr[1] / 256.0) - 32768.0
-                height_arr[row][column] = height  # if height > 0 else 0
-
-        plt.imshow(height_arr, cmap='terrain')
-        plt.colorbar()
-        plt.show()
+# -------- helper functions for sorting --------
+def sort_by_col(name):
+    return int(name.split('.')[0].split('-')[1])
 
 
-def plot_one():
-    os.chdir('out')
+def sort_by_row(name):
+    return int(name.split('.')[0].split('-')[2])
+# ----------------------------------------------
+
+
+def merge_all():
+    """
+    Merge all tiles for given zoom into one
+    """
+    os.chdir(OUTPUT_PATH)
+
+    # get all tiles and sort them
+    tiles_names_all = os.listdir()
+    tiles_names_all = sorted(tiles_names_all, key=sort_by_col)
+
+    # get number of first column
+    first_col_num = int(tiles_names_all[0].split('-')[1])
+
+    # split tiles names by cols
+    tiles_names = []
+    rows = []
+    for tile in tiles_names_all:
+        zoom, col, row = tile.split('.')[0].split('-')
+        if int(row) not in rows:
+            tiles_names.append([])
+            rows.append(int(row))
+        tiles_names[int(col) - first_col_num].append(tile)
+
+    # merge all tiles into one
+    all_tiles = None
+    for col in tiles_names:
+        col = sorted(col, key=sort_by_row)
+        col_tiles = None
+        for row in col:
+            im = cv2.imread(row, cv2.IMREAD_UNCHANGED)
+            if col_tiles is None:
+                col_tiles = im
+            else:
+                col_tiles = np.concatenate((col_tiles, im), axis=0)
+
+        if all_tiles is None:
+            all_tiles = col_tiles
+        elif col_tiles is not None:
+            all_tiles = np.concatenate((all_tiles, col_tiles), axis=1)
+
+    cv2.imwrite("all.png", all_tiles)
+    os.chdir('..')
+
+
+def plot_maps():
+    """
+    Convert each pixel from RGB to elevation. Display image
+    """
+    os.chdir(OUTPUT_PATH)
     im = cv2.imread('all.png', cv2.IMREAD_UNCHANGED)
     height_arr = np.zeros(im.shape[:2])
+
     for row in range(im.shape[0]):
         for column in range(im.shape[1]):
             bgr = im[row][column]
             height = (bgr[2] * 256.0 + bgr[1] + bgr[1] / 256.0) - 32768.0
             height_arr[row][column] = height if height > 0 else 0
 
-    plt.imsave("all2.png", height_arr, cmap='terrain')
+    os.chdir('..')
+
+    # normal map
+    plot_and_save(height_arr, "Height map", "height_map.png", 'terrain')
+
+    # normal map with pooling
+    pool_height_arr = skimage.measure.block_reduce(height_arr, (3, 3), np.mean)
+    plot_and_save(pool_height_arr, "Height map with pooling", "height_map_pooling.png", 'terrain')
+
+    # normal map split into groups
+    height_groups = {
+        0: 5,
+        1: 100,
+        2: 500,
+        3: 1200,
+        4: 2800,
+        5: 8000,
+    }
+    height_arr_groups = get_groups(height_arr, height_groups)
+    plot_and_save(height_arr_groups, "Height map groups", "height_map_groups.png")
+    pool_height_arr_groups = get_groups(pool_height_arr, height_groups)
+    plot_and_save(pool_height_arr_groups, "Height map groups with pooling", "height_map_groups_pooling.png")
+
+    # gradient
+    gradient_arr = abs(np.gradient(height_arr, axis=1))
+    plot_and_save(gradient_arr, "Gradient map", "gradient_map.png", 'terrain')
+
+    # gradient groups
+    gradient_groups = {
+        0: 2,
+        1: 10,
+        2: 50,
+        3: 150,
+        4: 500,
+        5: 5000
+    }
+    gradient_arr_groups = get_groups(gradient_arr, gradient_groups)
+    plot_and_save(gradient_arr_groups, "Gradient map groups", "gradient_map_groups.png")
+
+    plt.show()
+
+
+def plot_and_save(arr, title, file_name, cmap=None):
+    plt.figure()
+    plt.imsave(file_name, arr, cmap=cmap)
+    plt.imshow(arr)
+    plt.title(title)
+
+
+def get_groups(arr, groups):
+    groups_arr = np.zeros(arr.shape)
+    for row in range(arr.shape[0]):
+        for column in range(arr.shape[1]):
+            for key, val in groups.items():
+                if arr[row][column] < val:
+                    groups_arr[row][column] = int(key)
+                    break
+
+    return groups_arr
 
 
 def main():
+    if os.path.exists(OUTPUT_PATH):
+        shutil.rmtree(OUTPUT_PATH)
+
+    os.mkdir(OUTPUT_PATH)
+
     tiles = get_tiles(ZOOM, *BOUNDS)
     download(tiles)
-    # plot_all()
-    # plot_one()
+    merge_all()
+    plot_maps()
 
 
 if __name__ == '__main__':
